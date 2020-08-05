3bbf2b25636b46fae376889229b64d32d1ad8792
st-nann <st.sanyanee.com>
2020-08-06 02:42:38 +0700

    snackbar

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,34 @@
-FROM node:10.16-alpine
+# build stage
+
+## Pull Image
+FROM node:lts-alpine as build-stage
+
+## Create Path for App
 WORKDIR /app
+
+## Copy file
 COPY package*.json yarn.lock ./
-RUN yarn insatll
+
+## Install
+RUN yarn install
+
+## Copy file
 COPY . .
+
+## Build
 RUN yarn build
-EXPOSE 8080
+
+
+# production stage
+
+## Pull Image
+FROM nginx:stable-alpine as production-stage
+
+## Copy file
+COPY --from=build-stage /app/dist /usr/share/nginx/html
+
+## Setup port
+EXPOSE 80
+
+## Run Command
+CMD ["nginx", "-g", "daemon off;"]
--- src/store/modules/components/actions.ts
+++ src/store/modules/components/actions.ts
@@ -1,5 +1,6 @@
 import { State } from "@/store";
 import _ from "lodash";
+import { v4 as uuidv4 } from "uuid";
 import { ActionContext, ActionTree } from "vuex";
 import { mutationType } from "./mutation-types";
 import ComponentState from "./states";
@@ -10,6 +11,32 @@ const actions: ActionTree<ComponentState, State> = {
     data: boolean
   ) {
     commit(mutationType.LOADING, data);
+  },
+  updateSnackbar(
+    { commit, dispatch, state }: ActionContext<ComponentState, State>,
+    data: {
+      id: string
+      txt: string
+      type: string
+    }
+  ) {
+    const newSnackbar = state.snackbar || [];
+    newSnackbar.push(data);
+    data.id = uuidv4().toString();
+    commit(mutationType.SET_SNACKBAR, newSnackbar);
+    setTimeout(() => {
+      dispatch("closeSnackbar", data.id);
+    }, 5000);
+  },
+  closeSnackbar(
+    { commit, state }: ActionContext<ComponentState, State>,
+    id: string,
+  ): void {
+    const newSnackbar = state.snackbar || [];
+    commit(
+      mutationType.SET_SNACKBAR,
+      _.filter(newSnackbar, (snack) => snack.id !== id),
+    );
   }
 };
 
